38931b727d3f83829aac8ea43e53ef66cfcaa46c
Jadiger <jturganbaev1999@gmail.com>
2023-04-13 03:58:57 +0500

    Add Portfolio Section

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
+
 window.addEventListener('load',()=> {
     const body = document.querySelector('body');
           navLinks = document.querySelector('.nav__links'),
           burgerMenu = document.querySelector('.nav__burger'),
-          navLink = document.querySelectorAll('.nav__link')
+          navLink = document.querySelectorAll('.nav__link'),
+          portfolio = document.querySelector('.portfolio__items')
 
     function navLinksToggle() {
         burgerMenu.classList.toggle('burger_active')
@@ -17,4 +19,38 @@ window.addEventListener('load',()=> {
             navLinksToggle()
         })
     })
+    portfolios.forEach((item)=> {
+        portfolio.innerHTML +=`
+            <div class="portfolio__item">
+                <div class="portfolio__img">
+                    <img src=${item.image}>
+                </div>
+                <div class="portfolio__info">
+                    <h3 class="portfolio__name">${item.name}</h3>
+                    <p class="portfolio__desc">${item.desc}</p> 
+                    <div class="portfolio__techs">
+                    
+                    </div>
+                    <div class="portfolio__links">
+                    </div>
+            </div>
+        `
+    })
+    const portfolioTech = document.querySelectorAll('.portfolio__techs')
+        portfolios.forEach((portfolio)=> {
+            portfolio.technology.forEach(tech=> {
+                portfolioTech[portfolio.id-1].innerHTML += `
+                <span>${tech}</span>
+            `
+            })
+        } )
+    const portfolioLinks = document.querySelectorAll('.portfolio__links')
+        portfolios.forEach((portfolio)=> {
+            portfolio.links.forEach(link=> {
+                portfolioLinks[portfolio.id-1].innerHTML += `
+                <a href=${link.url}>${link.name}</a>
+            `
+            })
+        } )
+
 })
---
+++ js/portfolios.js
@@ -0,0 +1,39 @@
+const portfolios = [
+            {
+                id : 1,
+                name : 'Blog website',
+                desc : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                        industry's standard dummy text ever since the 1500s.`,
+                image :'./assets/portfolios-screen/jt-blogs.png',
+                technology : ['React Js', 'Sass(Scss)'],
+                links : [
+                    {
+                        name : 'Github',
+                        url : 'https://github.com/Jadiger/blog-website'
+                    },
+                    {
+                        name : 'Live Demo',
+                        url : 'https://jt-blogs.netlify.app'
+                    }
+                ]
+            },
+            {
+                id : 2,
+                name : 'Layout Website',
+                desc : `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the 
+                        industry's standard dummy text ever since the 1500s.`,
+                image :'./assets/portfolios-screen/layout.png',
+                technology : ['Vanilla Js', 'Css'],
+                links : [
+                    {
+                        name : 'Github',
+                        url : 'https://github.com/Jadiger/homework_challange'
+                    },
+                    {
+                        name : 'Live Demo',
+                        url : 'https://jtch.netlify.app'
+                    }
+                ]
+            }
+        ]
+
